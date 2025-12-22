d61b12160830a1776f96c8c917142bf0947dd833
Vishalji370 <vishalji370@gmail.com>
2025-12-22 19:15:09 +0530

    six commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -32,7 +32,7 @@ app.get("/", (req, res) => {
   res.send("🎬 MERN Movie API Running");
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = Number(process.env.PORT) || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
